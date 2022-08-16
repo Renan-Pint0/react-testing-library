219490aff3c833dea2f0d22faf20e14e064404cf
Renan Pinto <renanrnpinto@gmail.com>
2022-08-16 20:57:51 -0300

    Requisito 01

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,35 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Test the App page', () => {
+  test('Test the home link', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const homeLink = screen.getByRole('link', { name: /home/i });
+    expect(homeLink).toBeInTheDocument();
+    userEvent.click(homeLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+  test('Test the About link', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const aboutLink = screen.getByRole('link', { name: /about/i });
+    expect(aboutLink).toBeInTheDocument();
+    userEvent.click(aboutLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+  test('Test the Pokémons Favoritados link', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const favoritesLink = screen.getByRole('link', { name: /Favorite Pokémons/i });
+    expect(favoritesLink).toBeInTheDocument();
+    userEvent.click(favoritesLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+});
