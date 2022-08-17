cccfeda8300d0cac45cd7d17203e53798a18a808
Renan Pinto <renanrnpinto@gmail.com>
2022-08-17 09:57:48 -0300

    Requisito 03

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../pages/FavoritePokemons';
+
+describe('Test the Favorite Pokemons page', () => {
+  test('Test the page when are no favorite pokemons', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const noFavorite = screen.getByText(/No favorite pokemon found/i);
+    expect(noFavorite).toBeInTheDocument();
+  });
+});
