73809738b7c89aa40d9fd5876820c8711b20c2d6
Renan Pinto <renanrnpinto@gmail.com>
2022-08-17 10:14:48 -0300

    Requisito 04

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../pages/NotFound';
+
+describe('Test the not found page', () => {
+  test('Test the not found text', () => {
+    renderWithRouter(<NotFound />);
+    const notFoundText = screen.getByRole(
+      'heading', { name: /Page requested not found/i, level: 2 },
+    );
+    expect(notFoundText).toBeInTheDocument();
+  });
+  test('Test the not found image', () => {
+    renderWithRouter(<NotFound />);
+    const notFoundImg = screen.getByAltText(
+      /Pikachu crying because the page requested was not found/i,
+    );
+    expect(notFoundImg).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+    expect(notFoundImg).toBeInTheDocument();
+  });
+});
